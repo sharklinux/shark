9a14a264208b1eefe28ed62b93c077348f02ee9a
Nan Xiao <nan@chinadtrace.org>
2015-09-10 11:12:12 -0400

    Remove unused "open_raw_sock" function.

--- bpf/libbpf/libbpf.c
+++ bpf/libbpf/libbpf.c
@@ -106,30 +106,6 @@ int bpf_prog_load(enum bpf_prog_type prog_type,
 	return syscall(__NR_bpf, BPF_PROG_LOAD, &attr, sizeof(attr));
 }
 
-int open_raw_sock(const char *name)
-{
-	struct sockaddr_ll sll;
-	int sock;
-
-	sock = socket(PF_PACKET, SOCK_RAW | SOCK_NONBLOCK | SOCK_CLOEXEC, htons(ETH_P_ALL));
-	if (sock < 0) {
-		printf("cannot create raw socket\n");
-		return -1;
-	}
-
-	memset(&sll, 0, sizeof(sll));
-	sll.sll_family = AF_PACKET;
-	sll.sll_ifindex = if_nametoindex(name);
-	sll.sll_protocol = htons(ETH_P_ALL);
-	if (bind(sock, (struct sockaddr *)&sll, sizeof(sll)) < 0) {
-		printf("bind to %s: %s\n", name, strerror(errno));
-		close(sock);
-		return -1;
-	}
-
-	return sock;
-}
-
 int perf_event_open(struct perf_event_attr *attr, int pid, int cpu,
 		    int group_fd, unsigned long flags)
 {
